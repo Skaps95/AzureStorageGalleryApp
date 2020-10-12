d1a2b9a5e913b8f38675af2099d46856c50623ea
Sahaj Kapoor <sahajkapoor95@gmail.com>
2020-10-12 16:08:56 +0530

    from private cloud

--- SimpleImageGallery/Controllers/ImageController.cs
+++ SimpleImageGallery/Controllers/ImageController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -28,6 +29,7 @@ namespace SimpleImageGallery.Controllers
         public IActionResult Upload()
         {
             var model = new UploadImageModel();
+            //await DownloadImageToDisplay("interiordesk.jpg");
             return View(model);
         }
 
@@ -43,6 +45,40 @@ namespace SimpleImageGallery.Controllers
             await _imageService.SetImage(title, tags, blockBlob.Uri);
 
             return RedirectToAction("Index", "Gallery");
+        }
+
+        public async Task<IActionResult> DownloadImageToDisplay()
+        {
+            string filename = "interiordesk.jpg";
+            var container = _imageService.GetBlobContainer(AzureStorageConnectionString, "images");
+            if (await container.ExistsAsync())
+            {
+                var fileNm = filename.Trim('"');
+                var blobref = container.GetBlobReference(fileNm);
+                if (await blobref.ExistsAsync())
+                {
+                    MemoryStream ms = new MemoryStream();
+                    await blobref.DownloadToStreamAsync(ms);
+                    Stream blobStream = blobref.OpenReadAsync().Result;
+                    byte[] bytes;
+                    using (var memStream = new MemoryStream())
+                    {
+                        blobStream.CopyTo(memStream);
+                        bytes = memStream.ToArray();
+                    }
+                    string base64 = Convert.ToBase64String(bytes);
+                    ViewBag.base64 = "data: image / png; base64," + base64;
+                    return View();
+                }
+                else
+                {
+                    return Content("File does not exist");
+                }
+            }
+            else
+            {
+                return Content("Container does not exist");
+            }
         }
     }
 }
